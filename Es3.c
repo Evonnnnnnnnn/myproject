01c57d21f1ed6499267ef66796a305db21f45027
Evonnnnnnnnn <100685821+Evonnnnnnnnn@users.noreply.github.com>
2022-03-08 18:30:52 +0800

    Create Es3.c

---
+++ Es3.c
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int myAge = 20;
+  cout << "I am " <<15 << " years old. \n";
+  cout << "I am " <<  myAge << " years old. \n ";
+  return 0;
+}
