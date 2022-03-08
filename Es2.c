e623ee5a52b85c33198830907c755bcf223af6b9
Evonnnnnnnnn <100685821+Evonnnnnnnnn@users.noreply.github.com>
2022-03-08 18:26:57 +0800

    Create Es2.c

---
+++ Es2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+       
+    cout <<  "hello Nina ";
+    cout <<  "my height is " << 1 << "m " << 55 << "cm";
+
+    return 0;
+}
